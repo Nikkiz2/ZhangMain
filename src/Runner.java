325664a1be23345d9fce541dc322574909d9f174
BT_1N3_33 <sample@bths.edu>
2024-10-25 13:58:18 -0400

    initial commit

---
+++ src/Runner.java
@@ -0,0 +1,9 @@
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println("Today is not a good day");
+        System.out.println("But the next week is even worse");
+        System.out.println("Three tests next week");
+        System.out.println("DUN DUN");
+        System.out.println("THE S A T NEXT SATURDAY");
+    }
+}
