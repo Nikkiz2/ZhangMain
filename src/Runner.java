facd5eea498974150ef36f8dc4c1423a3437158e
BT_1N3_33 <sample@bths.edu>
2024-10-25 13:58:55 -0400

    initial commit

--- src/Runner.java
+++ src/Runner.java
@@ -5,5 +5,8 @@ public class Runner {
         System.out.println("Three tests next week");
         System.out.println("DUN DUN");
         System.out.println("THE S A T NEXT SATURDAY");
+        System.out.println("but its gonna be okay");
+        System.out.println("BECAUSE IM A RAT");
+        System.out.println("we'll cook the test up");
     }
 }
