2e640f954b1da4329e2025570b61d2a3fc9ae84f
BT_1N3_33 <sample@bths.edu>
2024-10-25 13:59:33 -0400

    changed a print statement

--- src/Runner.java
+++ src/Runner.java
@@ -1,6 +1,6 @@
 public class Runner {
     public static void main(String[] args) {
-        System.out.println("Today is not a good day");
+        System.out.println("Today is a terrible day");
         System.out.println("But the next week is even worse");
         System.out.println("Three tests next week");
         System.out.println("DUN DUN");
